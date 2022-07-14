6abf10641f60f5e2b68fe114ff3b1580cee186fd
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-15 02:50:11 +0530

    learning how to perform CRUD operations with MongoDB Atlas

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
-require("dotenv").config()
 const mongoose = require("mongoose");
 
+// Gives access to .env file and its content, so make sure to require it whenever using .env file content
+require("dotenv").config()
+
+// Accessing the DB_STRING environment variable in the .env file     
 const dbString = process.env.DB_STRING;
 console.log(dbString);
 
@@ -8,10 +11,12 @@ mongoose.connect(dbString, {
     useNewUrlParser: true,
 }).catch((error) => console.log(error));
 
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-    console.log("Database connection established successfully");
+const connection = mongoose.connect(dbString, {
+    useNewUrlParser: true,
 })
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((err) => console.log(err));
+
+console.log(connection);
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+require("dotenv").config();
 const connection = require("./config/db");
+
 
 const app = express();
 const port = 3000 || process.env.PORT;
+
+// Test --------------------------- Middlewares -----------------------------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+    }
+}))
 
 // Test -------------------------- The Server Side Code ----------------------------------
 app.get("/", (req, res, next) => {
@@ -13,4 +28,7 @@ app.get("/", (req, res, next) => {
 })
 
 app.listen(port, () => { console.log(`Server running on port ${port}`) });
+
+// The middleware for handling errrors, is always at the last
+app.use((error, req, res, next) => console.log(error));
 // Test -------------------------- Exporting the server side code ------------------------
